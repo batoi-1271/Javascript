7806a7dd0fd5480b87882be6d69cf2a30b5939a1
Ba Toi Nguyen <batoi1271.doe@gmail.com>
2022-07-04 15:27:15 +0700

    validate submit form

--- ThucHanh/validation/formF8/js.js
+++ ThucHanh/validation/formF8/js.js
@@ -1,72 +1,139 @@
 function Validator(options) {
+  var selectorRules = {};
 
+  function validate(inputElement, rule) {
+    var errorElement = inputElement.parentElement.querySelector(
+      options.errorSelector
+    );
+    var errorMessage;
 
-    function validate(inputElement, rule) {
-        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-        var errorMessage = rule.test(inputElement.value);
+    // Lay ra cac rule cua selector
+    var rules = selectorRules[rule.selector];
 
-        if(errorMessage) {
-            errorElement.innerText = errorMessage;
-            inputElement.parentElement.classList.add('invalid')
-        } else {
-            errorElement.innerText = '';
-            inputElement.parentElement.classList.remove('invalid')
-
-        }
+    // lap qua tung rule va kiem tra
+    // neu co loi thi dung viec kiem tra
+    for (var i = 0; i < rules.length; ++i) {
+      errorMessage = rules[i](inputElement.value);
+      if (errorMessage) break;
     }
 
-    // Lấy elements của form
-    var formElement = document.querySelector(options.form);
-
-    if(formElement) {
-        options.rules.forEach(function(rule) {
-            var inputElement = formElement.querySelector(rule.selector);
-
-            if(inputElement) {
-                // XỬ lí TH blur khỏi input
-                inputElement.onblur = function() {
-                    validate(inputElement, rule);
-                }
-
-                // XỬ lí khi người dùng nhập vào input
-                inputElement.oninput = function () {
-                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-
-                    errorElement.innerText = '';
-                    inputElement.parentElement.classList.remove('invalid')
-                }
-            }
-        })
+    if (errorMessage) {
+      errorElement.innerText = errorMessage;
+      inputElement.parentElement.classList.add("invalid");
+    } else {
+      errorElement.innerText = "";
+      inputElement.parentElement.classList.remove("invalid");
     }
 
-}
+    return !errorMessage;
+  }
 
-Validator.isRequired = function(selector) {
-    return {
-        selector,
-        test: function(value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+  // Lấy elements của form can validate
+  var formElement = document.querySelector(options.form);
+
+  if (formElement) {
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+
+      var isFormValid = true;
+
+      // thuc hien lap qua tung rule va validate
+      options.rules.forEach(function (rule) {
+        var inputElement = formElement.querySelector(rule.selector);
+        var isValid = validate(inputElement, rule);
+        if (!isValid) {
+          isFormValid = false;
         }
+      });
+
+     
+      if (isFormValid) {
+        // TH submit voi Javascript
+        if (typeof options.onSubmit === 'function') {
+
+          var enableInputs = formElement.querySelectorAll('[name]');
+          var formValues = Array.from(enableInputs).reduce(function (values, input) {
+            return (values[input.name] = input.value) && values;
+          }, {})
+
+          // console.log(formValues);
+          options.onSubmit(formValues);
+          // TH submit voi hanh vi mac dinh
+        } 
+      }
     }
+
+    // Lặp qua mỗi rule và xử lý (lắng nghe sự kiện blur, input, ..)
+    options.rules.forEach(function (rule) {
+      // luu lai cac rules cho moi inputs
+      // selectorRules[rule.selector] = rule.test;
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+
+      var inputElement = formElement.querySelector(rule.selector);
+
+      if (inputElement) {
+        // XỬ lí TH blur khỏi input
+        inputElement.onblur = function () {
+          validate(inputElement, rule);
+        };
+
+        // XỬ lí khi người dùng nhập vào input
+        inputElement.oninput = function () {
+          var errorElement = inputElement.parentElement.querySelector(
+            options.errorSelector
+          );
+
+          errorElement.innerText = "";
+          inputElement.parentElement.classList.remove("invalid");
+        };
+      }
+    });
+  }
 }
 
-Validator.isEmail = function(selector) {
-    return {
-        selector,
-        test: function(value) {
-            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined : 'Trường này phải là email';
-        }
+Validator.isRequired = function (selector, message) {
+  return {
+    selector,
+    test: function (value) {
+      return value.trim() ? undefined : message || "Vui lòng nhập trường này";
+    },
+  };
+};
 
-    }    
-}
+Validator.isEmail = function (selector, message) {
+  return {
+    selector,
+    test: function (value) {
+      var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return regex.test(value)
+        ? undefined
+        : message || "Trường này phải là email";
+    },
+  };
+};
 
-Validator.minLength = function(selector, min) {
-    return {
-        selector,
-        test: function(value) {
-            return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min}`;
-        }
+Validator.minLength = function (selector, min, message) {
+  return {
+    selector,
+    test: function (value) {
+      return value.length >= min
+        ? undefined
+        : message || `Vui lòng nhập tối thiểu ${min}`;
+    },
+  };
+};
 
-    }    
-}
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector,
+    test: function (value) {
+      return value === getConfirmValue()
+        ? undefined
+        : message || "Giá trị nhập vào không đúng";
+    },
+  };
+};
