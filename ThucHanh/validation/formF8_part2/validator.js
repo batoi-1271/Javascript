b239ddea90e208418dfc47e38146a57b5c9baa6f
Ba Toi Nguyen <batoi1271.doe@gmail.com>
2022-07-06 17:13:01 +0700

    form-2

---
+++ ThucHanh/validation/formF8_part2/validator.js
@@ -0,0 +1,58 @@
+function Validator(formSelector) {
+
+    var formRules = {};
+    /**
+     * Quy ước tạo rule
+     * - Nếu có lỗi thì return `error message`
+     * - Nếu không có lỗi thì return `undefined`
+     */
+    var validatorRules = {
+        required: function(value) {
+            return value ? undefined : 'Vui lòng nhập trường này';
+        },
+        email: function(value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : "Trường này phải là email";
+        },
+        min: function(min) {
+            return function(value) {
+                return value.length >= min ? undefined : `Vui lòng nhập it nhất ${min} kí tự`;
+            }
+        },
+        max: function(max) {
+            return function(value) {
+                return value.length <= max ? undefined : `Vui lòng nhập tối đa ${max} kí tự`;
+            }
+        },
+    };
+
+
+
+    // Lấy ra form Element trong DOM theo `formSelector`
+    var formElement = document.querySelector(formSelector);
+
+    // Chỉ xử lý khi có element trong DOM
+    if (formElement) {
+        var inputs = formElement.querySelectorAll('[name][rules]');
+        for (var input of inputs) {
+
+            var rules = input.getAttribute('rules').split('|');
+            for (var rule of rules) {
+
+                if (rule.includes(':')) {
+                    var ruleInfo = rule.split(':');
+                    rule = ruleInfo[0];
+                }
+
+                if (Array.isArray(formRules[input.name])) {
+                    formRules[input.name].push(validatorRules[rule]);
+                } else {
+                    formRules[input.name] = [validatorRules[rule]];
+                }
+            }
+
+        }
+        console.log(formRules);
+
+    }
+}
